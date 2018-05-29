75a93e45bc4f82b6ed0d40ef28f7ad3ecdb56c72
Alyona <alyona-gra98@yandex.ru>
2018-05-29 23:12:04 +0300

    sphere

--- TomogramVisualizer/Bin.cs
+++ TomogramVisualizer/Bin.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace TomogramVisualizer
 {
@@ -11,24 +13,31 @@ namespace TomogramVisualizer
     {
         public static int X, Y, Z;
         public static short[,,] array;
+        public static Bitmap bitmap;
 
         public static void ReadBin(string path)
         {
-            if (File.Exists(path))
+            if (File.Exists(path))////
             {
-                BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
+                //BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
 
-                X = reader.ReadInt32();
-                Y = reader.ReadInt32();
-                Z = reader.ReadInt32();
+                //X = reader.ReadInt32();
+                //Y = reader.ReadInt32();
+                //Z = reader.ReadInt32();
 
-                array = new short[X, Y, Z];
-                for (int k = 0; k < Z; k++)
-                    for (int j = 0; j < Y; j++)
-                        for (int i = 0; i < X; i++)
-                        {
-                            array[i, j, k] = reader.ReadInt16();
-                        }
+                //array = new short[X, Y, Z];
+                //for (int k = 0; k < Z; k++)
+                //    for (int j = 0; j < Y; j++)
+                //        for (int i = 0; i < X; i++)
+                //        {
+                //            array[i, j, k] = reader.ReadInt16();
+                //        }
+
+                bitmap = new Bitmap(path);
+
+                X = bitmap.Width;
+                Y = bitmap.Height;
+                Z = 1;
             }
         }
     }
--- TomogramVisualizer/View.cs
+++ TomogramVisualizer/View.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK.Graphics.OpenGL;
+using OpenTK;
 using System.Drawing;
 using System.Drawing.Imaging;
 
@@ -12,13 +13,15 @@ namespace TomogramVisualizer
     public class View
     {
         public static int Min = 0, Max = 120;
+        public int N=0;
 
         public void SetupView(int w, int h)
         {
+            N = Bin.X;////
             GL.ShadeModel(ShadingModel.Smooth);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
-            GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
+            GL.Ortho(-Bin.X, Bin.X, -Bin.Y, Bin.Y, -N, N);////
             GL.Viewport(0, 0, w, h);
         }
 
@@ -35,17 +38,51 @@ namespace TomogramVisualizer
             return v;
         }
 
+        public List<double> F(double u, double v)////
+        {
+            List<double> l = new List<double>();
+            double r = Math.Min(Math.Min(Bin.X, Bin.Y), N) * 0.7;
+            l.Add(Math.Cos(u) * Math.Sin(v) * r);
+            l.Add(Math.Cos(v) * r);
+            l.Add(Math.Sin(u) * Math.Sin(v) * r);
+            return l;
+        }
+
+        public void DrawSphere()////
+        {
+            double UResolution =10, VResolution = 10;
+            double startU = 0, startV = 0;
+            double endU = Math.PI * 2;
+            double endV = Math.PI;
+            double stepU = (endU - startU) / UResolution; // step size between U-points on the grid 
+            double stepV = (endV - startV) / VResolution; // step size between V-points on the grid 
+            for (int i = 0; i < UResolution; i++)
+            {
+                for (double j = 0; j < VResolution; j++)
+                {
+                    double u = i * stepU + startU;
+                    double v = j * stepV + startV;
+                    double un = (i + 1 == UResolution) ? endU : (i + 1) * stepU + startU;
+                    double vn = (j + 1 == VResolution) ? endV : (j + 1) * stepV + startV;
+                    List<double> p0 = F(u, v);
+                    List<double> p1 = F(u, vn);
+                    List<double> p2 = F(un, v);
+                    List<double> p3 = F(un, vn);
+                    DrawTextureRectGL3d(p0, p1, p2, p3);
+                }
+            }
+        }
+
+
         public void DrawQuadr(int ln)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.Begin(BeginMode.Quads);
 
-            for (int x = 0; x < Bin.X - 1; x++)
-                for (int y = 0; y < Bin.Y - 1; y++)
-                {
-                    DrawRectGL(x, y, x, y + 1, x + 1, y + 1, x + 1, y, ln);
-                }
-              
+            ////DrawSphere();
+            //DrawRectGL((int)0.1*Bin.X, (int)0.1 * Bin.Y, (int)0.1 * Bin.X, (int)0.8 * Bin.Y, 
+            //    (int)0.8 * Bin.X, (int)0.8 * Bin.Y, (int)0.8 * Bin.X, (int)0.1 * Bin.Y, 4);
+
             GL.End();
         }
 
@@ -73,12 +110,13 @@ namespace TomogramVisualizer
 
         }
 
-        public void GenerateTextureImage(int ln)
+        public void GenerateTextureImage(int ln)////
         {
-            textureImage = new Bitmap(Bin.X, Bin.Y);
-            for (int i = 0; i < Bin.X; i++)
-                for (int j = 0; j < Bin.Y; j++)
-                    textureImage.SetPixel(i, j, TransferFunction(Bin.array[i, j, ln]));
+            //textureImage = new Bitmap(Bin.X, Bin.Y);
+            //for (int i = 0; i < Bin.X; i++)
+            //    for (int j = 0; j < Bin.Y; j++)
+            //        textureImage.SetPixel(i, j, TransferFunction(Bin.array[i, j, ln]));
+            textureImage = Bin.bitmap;
         }
 
         public void DrawTexture()
@@ -88,7 +126,7 @@ namespace TomogramVisualizer
             GL.BindTexture(TextureTarget.Texture2D, VBOtexture);
 
             GL.Begin(BeginMode.Quads);
-            DrawTextureRectGL();
+            DrawSphere();////
             GL.End();
 
             GL.Disable(EnableCap.Texture2D);
@@ -110,6 +148,7 @@ namespace TomogramVisualizer
             GL.Vertex2(x4, y4);
         }
 
+
         private void DrawTextureRectGL()
         {
             GL.Color3(Color.White);
@@ -125,6 +164,23 @@ namespace TomogramVisualizer
 
             GL.TexCoord2(1f, 0f);
             GL.Vertex2(Bin.X, 0);
+        }
+
+        private void DrawTextureRectGL3d(List<double> p0, List<double> p1, List<double> p2, List<double> p3)////
+        {
+            GL.Color3(Color.White);
+
+            GL.TexCoord2(0f, 0f);
+            GL.Vertex3(p0[0], p0[1], p0[2]);
+
+            GL.TexCoord2(0f, 1f);
+            GL.Vertex3(p1[0], p1[1], p1[2]);
+
+            GL.TexCoord2(1f, 1f);
+            GL.Vertex3(p3[0], p3[1], p3[2]);
+
+            GL.TexCoord2(1f, 0f);
+            GL.Vertex3(p2[0], p2[1], p2[2]);
         }
     }
 }
